d227456e50b0db1ff386a1a8782b3cb7521a0e1a
Piotr Fryc <piotr.fryc@gmail.com>
2022-06-14 23:01:49 +0200

    [code] Add support for calculating frame features for multiple frames

--- features.py
+++ features.py
@@ -1,9 +1,21 @@
 import numpy as np
 
 
+def flatten(d):
+    result = {}
+    for key, value in d.items():
+        if isinstance(value, dict):
+            for inner_key, inner_value in value.items():
+                result[f"{key}_{inner_key}"] = inner_value
+        else:
+            result[key] = value
+    return result
+
+
 class Features:
     """
     Implement new feature by defining method with name starting with feature_ that returns a scalar.
+    If the method name starts with feature_frame_ it will be treated as frame based feature and calculated for each frame time defined by the parameter to __call__ method.
     """
 
     def __init__(self) -> None:
@@ -21,7 +33,7 @@ class Features:
     def feature_ratio(self, video):
         return video.get_width() / video.get_height()
 
-    def feature_saturated_red(self, video):
+    def feature_frame_saturated_red(self, video, point):
         hist, _ = video.get_r_histogram()
         return hist[-1]
 
@@ -59,9 +71,25 @@ class Features:
         hist, _ = video.get_y_histogram()
         return hist[-1]
 
-    def __call__(self, video):
-        return {
-            feature_function[8:]: getattr(self, feature_function)(video)
-            for feature_function in dir(self)
-            if feature_function.startswith("feature_")
-        }
+    def __wrapper(self, video, function_name, calc_points):
+        if function_name.startswith("feature_frame_"):
+            for time_point in calc_points:
+                return getattr(self, function_name)(video, time_point)
+        elif function_name.startswith("feature_"):
+            return getattr(self, function_name)(video)
+
+    def __call__(self, video, calc_points=[1, -1]):
+        return flatten(
+            {
+                feature_function_name[8:]: {
+                    str(timepoint): getattr(self, feature_function_name)(
+                        video, timepoint
+                    )
+                    for timepoint in calc_points
+                }
+                if feature_function_name.startswith("feature_frame_")
+                else getattr(self, feature_function_name)(video)
+                for feature_function_name in dir(self)
+                if feature_function_name.startswith("feature_")
+            }
+        )
